0f7e5083a065576918128a1c5acaab55cdd270c1
Sharwani <shrawani.d.vaidya@gmail.com>
2021-11-01 13:43:35 +0530

    Added forms

---
+++ src/survery-form.js
@@ -0,0 +1,99 @@
+import home from './images/home.png'
+
+function SurveryForm(){
+    return (
+        <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-col  w-full mb-12">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Survery</h1>
+           
+            <p style={{marginBottom:"15px"}} class="sm:text-xl text-xl font-medium text-left text-black-900">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <div class="flex items-center mb-4">
+                <input id="country-option-1" type="radio" name="countries" value="Yes" class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Yes
+                </label>
+                <input id="country-option-1" type="radio" name="countries" value="No" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                No
+                </label>
+                <input id="country-option-1" type="radio" name="countries" value="Maybe" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Maybe
+                </label>
+            </div>
+
+            <p style={{marginBottom:"15px", marginTop:"10px"}} class="sm:text-xl text-xl font-medium text-left text-black-900">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <div class="flex items-center mb-4">
+                <input id="country-option-1" type="radio" name="countries1" value="Yes" class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Yes
+                </label>
+                <input id="country-option-1" type="radio" name="countries1" value="No" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                No
+                </label>
+                <input id="country-option-1" type="radio" name="countries1" value="Maybe" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Maybe
+                </label>
+            </div>
+            <p style={{marginBottom:"15px", marginTop:"10px"}} class="sm:text-xl text-xl font-medium text-left text-black-900">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <div class="flex items-center mb-4">
+                <input id="country-option-1" type="radio" name="countries2" value="Yes" class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Yes
+                </label>
+                <input id="country-option-1" type="radio" name="countries2" value="No" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                No
+                </label>
+                <input id="country-option-1" type="radio" name="countries2" value="Maybe" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Maybe
+                </label>
+            </div>
+            <p style={{marginBottom:"15px", marginTop:"10px"}} class="sm:text-xl text-xl font-medium text-left text-black-900">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <div class="flex items-center mb-4">
+                <input id="country-option-1" type="radio" name="countries4" value="Yes" class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Yes
+                </label>
+                <input id="country-option-1" type="radio" name="countries4" value="No" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                No
+                </label>
+                <input id="country-option-1" type="radio" name="countries4" value="Maybe" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Maybe
+                </label>
+            </div>
+            <p style={{marginBottom:"15px", marginTop:"10px"}} class="sm:text-xl text-xl font-medium text-left text-black-900">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <div class="flex items-center mb-4">
+                <input id="country-option-1" type="radio" name="countries5" value="Yes" class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Yes
+                </label>
+                <input id="country-option-1" type="radio" name="countries5" value="No" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                No
+                </label>
+                <input id="country-option-1" type="radio" name="countries5" value="Maybe" style={{marginLeft:"10px"}} class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" aria-labelledby="country-option-1" aria-describedby="country-option-1" />
+                <label for="country-option-1" class="text-sm font-medium text-gray-900 ml-2 block">
+                Maybe
+                </label>
+            </div>
+            <div class="p-2 w-full">
+                 <a class="inline-block font-semibold px-4 py-2 text-white bg-blue-600 md:text-white border border-white rounded focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                  href="#" style={{margiLeft: "280px"}}>Submit</a>
+              </div>
+            <div style={{marginLeft: "90%"}} class="inline-block font-semibold px-4 py-2 text-white bg-blue-600 md:bg-transparent md:text-white border border-white rounded">
+                <a href="#"><img src={home} alt="" style={{width:"50px", height:"50px"}}/></a>
+            </div>
+          </div>
+        </div>
+       
+        </section>
+    )
+}
+export default SurveryForm
